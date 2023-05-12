6aede4d6ba2a702a61a56234d2fa5b4d4918fa62
BohdanUstianivskyi <bohdan.ustianivskyi.tr.2021@lpnu.ua>
2023-05-12 21:30:04 +0300

    Lab04

--- src/Massif_01.java
+++ src/Massif_01.java
@@ -1,14 +1,12 @@
 public class Massif_01 {
     public static void main(String[] args) {
-        int[] evenNumbers = new int[50]; // ініціалізація масиву
+        int[] evenNumbers = new int[50];
 
         int number = 0;
         for (int i = 0; i < 50; i++) {
             evenNumbers[i] = number;
             number += 2;
         }
-
-// вивід масиву на консоль
         for (int i = 0; i < evenNumbers.length; i++) {
             System.out.print(evenNumbers[i] + " ");
         }
--- src/One_DimensionalArray.java
+++ src/One_DimensionalArray.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class One_DimensionalArray {
-}
+import java.util.Scanner;
+public class One_DimensionalArray {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the number of array elements: ");
+            int n = scanner.nextInt();
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++) {
+                System.out.print("Enter a number: ");
+                array[i] = scanner.nextDouble();
+            }
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += array[i];
+            }
+            System.out.println("The sum of array elements: " + sum);
+        }
+    }
+
+
